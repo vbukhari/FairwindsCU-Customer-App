ec6189028d2d3789a039e3d8ef6d4c61c18d7391
vbukhari <vbukhari@mail.usf.edu>
2022-07-18 00:18:44 -0400

    added enviroment config file

---
+++ src/app/config/enviroment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    api_url: 'https:/my.api.mockaroo.com/customers.json?key=e95894a0'
+};
--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,28 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../config/enviroment';
 import { Customer } from '../models/customer/customer.model';
 
-const apiKey = 'e95894a0';
-const url = `https:/my.api.mockaroo.com/customers.json?key=${apiKey}`;
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
+  private url = environment.api_url;
+  
   constructor(private http: HttpClient) { }
 
   createCustomer(customer:any): Observable<any>{
-    return this.http.post(url, customer);
+    return this.http.post(this.url, customer);
   }
 
   getCustomersBySize(size:number): Observable<Customer[]> {
-      return this.http.get<Customer[]>(`${url}&size=${size}`);
+      return this.http.get<Customer[]>(`${this.url}&size=${size}`);
   }
 
   getAllCustomers(): Observable<Customer[]> {
-    return this.http.get<Customer[]>(url);
+    return this.http.get<Customer[]>(this.url);
   }
 
 }
